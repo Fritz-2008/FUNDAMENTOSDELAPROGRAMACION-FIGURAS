865f4e617d172fb12ab0b6b8385c9af62b30ced2
Fritz-2008 <fritzanimaciones@gmail.com>
2026-05-11 15:16:07 -0500

    Create Concatenacion.java

---
+++ Concatenacion.java
@@ -0,0 +1,15 @@
+class Concatenacion { 
+
+    public static void main(String[] args) { 
+
+        String nombre= "Fritz"; 
+
+        String primerApellido = "Atachagua"; 
+
+        String nombreconApellido = nombre + " " + primerApellido; 
+
+        System.out.println(nombreconApellido); 
+
+    }   //  Fin del método main 
+
+}    //   Fin de la clase Concatenacion 
