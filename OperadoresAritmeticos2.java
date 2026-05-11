ddedca22bbe2417a8e2a4c0cbd446a4140adc1f8
Fritz-2008 <fritzanimaciones@gmail.com>
2026-05-11 15:25:04 -0500

    Create OperadoresAritmeticos2.java

---
+++ OperadoresAritmeticos2.java
@@ -0,0 +1,24 @@
+class OperadoresAritmeticos {
+    public static void main(String[] args) {
+        double numero1 = 7.0;
+        double numero2 = 5.0;
+
+        System.out.println("Operaciones con \"numero1\" y \"numero2\":");
+
+        // Suma
+        System.out.println(numero1 + numero2);
+
+        // Resta
+        System.out.println(numero1 - numero2);
+
+        // Multiplicación
+        System.out.println(numero1 * numero2);
+
+        // División
+        System.out.println(numero1 / numero2);
+
+        // Módulo (Residuo)
+        System.out.println(numero1 % numero2);
+
+    } // Fin del método main
+} // Fin de la clase OperadoresAritmeticos
